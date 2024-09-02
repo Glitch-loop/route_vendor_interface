8f71ea348e24acd97d9262874feeb4670ffa4d84
Glitch-loop <renetdejesus@gmail.com>
2024-09-02 11:02:54 -0600

    other commit

--- utils/moment_format.ts
+++ utils/moment_format.ts
@@ -2,6 +2,5 @@ import moment from 'moment';
 
 export function standard_format() {
   moment.locale('es');
-  console.log(moment().format('dddd, DD-MMM-YY'));
   return moment().format('dddd, DD-MMM-YY');
 }
